a3f628dd016128e17e7e2d0013b56f06da7ef4ab
Asha <ashayadav.ash@gmail.com>
2022-07-20 22:33:41 +0530

    commit- Changes in naming conventions

--- sample/src/test/java/classes/validateJson.java
+++ sample/src/test/java/classes/validateJson.java
@@ -12,42 +12,42 @@ public class validateJson extends Baseclass {
 
 	//  Validate team  has only 4 foreign players
     @Test
-    public void validateJson_1() {
+    public void validate_no_of_Foreign_Players() {
 
         JsonPath j = new JsonPath(resp.asString());
-        int count = j.getInt("player.size()");
-        int players = 0;
-        System.out.println("Total no of players: "+count);
+        int total_players = j.getInt("player.size()");
+        int Foreign_players = 0;
+        System.out.println("Total no of players: "+total_players);
 
-        for (int i = 0; i < count; i++) {
+        for (int i = 0; i < total_players; i++) {
             String country = j.getString("player.country[" + i + "]");
             if (!(country.equals("India")))
-                players++;
+            	Foreign_players++;
         }
 
-        System.out.println("Total no of Foreign Players are: " + players);
-        assertThat(players, equalTo(4));
+        System.out.println("Total no of Foreign Players are: " + Foreign_players);
+        assertThat(Foreign_players, equalTo(4));
         System.out.println("Validated:Team has only 4 foreign players");
     }
 
 
-    //   validate there is atleast  one wicket keeper
+    //   validate there is atleast one wicket keeper
     @Test
-    public void wicket_keeper() {
+    public void no_of_wicket_keeper() {
         JsonPath j = new JsonPath(resp.asString());
 
-        int count = j.getInt("player.size()");
-        int players = 0;
-        for (int i = 0; i < count; i++) {
+        int total_players = j.getInt("player.size()");
+        int wicket_keepers = 0;
+        for (int i = 0; i < total_players; i++) {
 
             String role = j.getString("player.role[" + i + "]");
 
             if (role.equals("Wicket-keeper"))
-                players++;
+            	wicket_keepers++;
 
         }
-        System.out.println("\n\nTotal no of wicket-Keepers : "+players);
-        assertThat(players,greaterThanOrEqualTo(1));
+        System.out.println("\n\nTotal no of wicket-Keepers : "+wicket_keepers);
+        assertThat(wicket_keepers,greaterThanOrEqualTo(1));
         System.out.println("Validated:Team  has atleast one Wicket-Keeper");
 
     }
